7ea993afb8e58fc0e862017b374564d27a386a0a
Roya Lotfi <lotfir@myumanitoba.ca>
2020-10-29 21:10:13 -0500

    we added a list of lendings in class staff since one staff can do the act of lending multiple times

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace MyLibrary.Dal.Models
 {
-    class Staff
+    class Staff : EntityBase
     {
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public List<Lending> Lendings { get; set; }
     }
 }
