957e543f559ba5ae65ae12b9c69619b849e1d25e
Roya Lotfi <lotfir@myumanitoba.ca>
2020-10-29 21:30:33 -0500

    filled the class entity base

--- Models/EntityBase.cs
+++ Models/EntityBase.cs
@@ -6,5 +6,14 @@ namespace MyLibrary.Dal.Models
 {
     class EntityBase
     {
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDatetime { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public DateTime? UpdatedDatetime { get; set; }
+
+        
     }
 }
